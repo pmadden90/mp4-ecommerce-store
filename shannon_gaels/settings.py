39e35edf26518f6306331568fb862d0e2ba462ef
Paul Callaghan <p.madden90@gmail.com>
2020-08-30 18:09:34 +0000

    Added staticroot to settings.py

--- shannon_gaels/settings.py
+++ shannon_gaels/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', )
+# SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'collectstaticneedsaprintedsecretkey'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
@@ -177,6 +179,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
