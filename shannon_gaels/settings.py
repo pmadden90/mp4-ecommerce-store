bdc674dfbef15e6283439d34ea67b50fd2e28ec5
Paul Callaghan <p.madden90@gmail.com>
2020-08-30 16:45:22 +0000

    REmoved secret key

--- shannon_gaels/settings.py
+++ shannon_gaels/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qkqn8hetzviuzji#p_re52r9wtzjmzm#8n+f-4s2b-qs+2m-m6'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ci-mp4-shannongaels.herokuapp.com', 'localhost']
 
